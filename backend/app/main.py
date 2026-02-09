7dc41f28b78efc1fb237f6bc2d3bcbaa32ecee14
Prajyot <prajyotborikar22@gmail.com>
2026-02-09 19:44:08 +0530

    minor change

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 from app.api.message import router as message_router
 from app.api.policy import router as policy_router
+from app.api.policy import lifespan
 
-app = FastAPI(title="Customer Success Orchestrator")
+app = FastAPI(title="Customer Success Orchestrator", lifespan=lifespan)
 
 # app.include_router(message_router)
 app.include_router(policy_router)
